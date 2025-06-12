3db33adca8771db86daff04410651684eea47798
SURAJ BORKUTE <150571151+Surajkecode@users.noreply.github.com>
2025-06-12 20:48:13 +0530

    Add files via upload

---
+++ my-app/app/assistant.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { AssistantRuntimeProvider } from "@assistant-ui/react";
+import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
+import { Thread } from "@/components/assistant-ui/thread";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+
+export const Assistant = () => {
+  const runtime = useChatRuntime({
+    api: "/api/chat",
+  });
+
+  return (
+    <AssistantRuntimeProvider runtime={runtime}>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink 
+                  href="#"
+                    className="text-2xl font-extrabold bg-gradient-to-r from-fuchsia-500 via-orange-400 to-yellow-300 dark:from-blue-400 dark:via-pink-500 dark:to-red-500 bg-clip-text text-transparent animate-gradient">
+                   Welcome To SURAJ GPT
+                  
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage
+                    className="text-base font-semibold text-purple-800 dark:text-purple-600">
+                    ASK QUESTION
+                  </BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </header>
+          <Thread />
+        </SidebarInset>
+      </SidebarProvider>
+    </AssistantRuntimeProvider>
+  );
+};
